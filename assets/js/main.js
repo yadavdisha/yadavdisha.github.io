a9af6096b481779421865149fc15c86bc3469845
Disha <dishayadav303@gmail.com>
2023-07-02 18:52:05 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,10 +37,25 @@
   /**
    * Easy on scroll event listener 
    */
+
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
   }
-
+/**
+   * Intro type effect
+   */
+  const typed = select('.typed')
+  if (typed) {
+    let typed_strings = typed.getAttribute('data-typed-items')
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000
+    });
+  }
   /**
    * Navbar links active state on scroll
    */
