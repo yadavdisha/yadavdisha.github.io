afdfbdb159652b59d15516bbe074ac9f364d4061
Disha <dishayadav303@gmail.com>
2023-07-03 15:01:39 -0400

    Update particles.js

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -27,13 +27,13 @@ var pJS = function(tag_id, params){
         }
       },
       color: {
-        value: '#fff'
+        value: '#00bcd4'
       },
       shape: {
         type: 'circle',
         stroke: {
           width: 0,
-          color: '#ff0000'
+          color: '#000000'
         },
         polygon: {
           nb_sides: 5
@@ -66,8 +66,8 @@ var pJS = function(tag_id, params){
       },
       line_linked: {
         enable: true,
-        distance: 100,
-        color: '#fff',
+        distance: 150,
+        color: '#00bcd4',
         opacity: 1,
         width: 1
       },
@@ -475,7 +475,7 @@ var pJS = function(tag_id, params){
         }else{
           var img_obj = pJS.tmp.img_obj;
         }
-
+3
         if(img_obj){
           draw();
         }
